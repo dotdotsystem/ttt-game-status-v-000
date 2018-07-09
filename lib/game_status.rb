cc0f2d78d5c921aec6bfcedb8c95a4ec6bb5777a
Claire Harmer <claire.f.harmer@gmail.com>
2018-07-09 22:42:30 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -29,7 +29,7 @@ def won?(board)
 
   def full?(board)
 WIN_COMBINATIONS.any? do |full|
-  board[full[0]] == position_taken?(board, full[0])
+  board[full[0]] == position_taken?(board, full[0]) && board[full[0]] != board[full[1]]
     end
   end
 
