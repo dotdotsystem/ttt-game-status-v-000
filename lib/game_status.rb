875a877600ed05caee4e2db5160ce03a2eb7f9ae
Claire Harmer <claire.f.harmer@gmail.com>
2018-07-09 21:53:29 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,6 +1,35 @@
-# Helper Method
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
+ require 'pry'
 
-# Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+ [0,1,2],
+ [3,4,5], 
+ [6,7,8],
+ [0,3,6], 
+ [1,4,7], 
+ [2,5,8], 
+ [2,4,6], 
+ [0,4,8]
+ ]
+ 
+ def full?(board)
+      WIN_COMBINATIONS.all do |full| 
+        board[full] == position_taken?(board, index)
+      binding.pry
+    end
+  end
+  
+  
+def won?(board)
+  WIN_COMBINATIONS.detect do |win_combo|
+    board[win_combo[0]] == board[win_combo[1]] && board[win_combo[1]] == board[win_combo[2]] && position_taken?(board, win_combo[0]) 
+   
+    end  
+  end 
+ 
+
+  
+
+ 
